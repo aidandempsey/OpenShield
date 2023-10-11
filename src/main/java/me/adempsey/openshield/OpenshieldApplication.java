be1d6f435fbb73cb0aac24be873c7488e3f90910
Aidan Dempsey <aidandempsey2019@gmail.com>
2023-10-11 10:10:39 +0100

    initialising spring boot application

---
+++ src/main/java/me/adempsey/openshield/OpenshieldApplication.java
@@ -0,0 +1,13 @@
+package me.adempsey.openshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/adempsey/openshield/OpenshieldApplicationTests.java
@@ -0,0 +1,13 @@
+package me.adempsey.openshield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenshieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
