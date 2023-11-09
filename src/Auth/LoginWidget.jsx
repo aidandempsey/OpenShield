cfb2e62e56ad78a851b0d48b0fbf7c6f36515ffd
Aidan Dempsey <aidandempsey2019@gmail.com>
2023-11-09 21:50:49 +0000

    set up okta login page

--- src/Auth/LoginWidget.jsx
+++ src/Auth/LoginWidget.jsx
@@ -1,23 +1,27 @@
-import { Redirect } from "react-router-dom"
-import { useOktaAuth } from "@okta/okta-react"
+import { Navigate } from 'react-router-dom';
+import { useOktaAuth } from '@okta/okta-react';
+import OktaSignInWidget from './OktaSignInWidget';
 
-export default function LoginWidget() {
-    const { oktaAuth, authState } = useOktaAuth()
-    const onSuccess = tokens => {
-        oktaAuth.handleLoginRedirect(tokens)
-    }
+const LoginWidget = ({ config }) => {
+    const { oktaAuth, authState } = useOktaAuth();
+    const onSuccess = (tokens) => {
+        oktaAuth.handleLoginRedirect(tokens);
+    };
 
-    const onError = err => {
-        console.log(err)
+    const onError = (err) => {
+        console.log('Sign in error: ', err);
     }
 
     if (!authState) {
         return (
             <p>Loading</p>
-        )
+        );
     }
 
-    return (
-        <div>LoginWidget</div>
-    )
-}
+    return authState.isAuthenticated ?
+        <Navigate to={{ pathname: '/' }} />
+        :
+        <OktaSignInWidget config={config} onSuccess={onSuccess} onError={onError} />;
+};
+
+export default LoginWidget;
--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Routes, Route, useNavigate } from "react-router-dom"
 
 // styles
 import "./App.css"
@@ -7,17 +7,36 @@ import "./App.css"
 import Dashboard from "./pages/dashboard/Dashboard";
 import Navbar from "./components/Navbar/Navbar";
 
+// okta
+import { oktaConfig } from "../lib/oktaConfig";
+import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js"
+import { Security, LoginCallback } from "@okta/okta-react"
+import LoginWidget from "../Auth/LoginWidget";
+
 function App() {
+  const navigate = useNavigate()
+  const oktaAuth = new OktaAuth(oktaConfig)
+
+  const customAuthHandler = () => {
+    navigate("/login")
+  }
+
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    navigate(toRelativeUrl(originalUri || '/', window.location.origin), { replace: true })
+  }
+
   return (
     <div className="App">
-      <Navbar />
-      <BrowserRouter>
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
+        <Navbar />
         <div className="container">
           <Routes>
-            <Route exact path='/' element={<Dashboard />} />
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<LoginWidget config={oktaConfig} />} />
+            <Route path="/login/callback" element={<LoginCallback />} />
           </Routes>
         </div>
-      </BrowserRouter>
+      </Security>
     </div>
   );
 }
