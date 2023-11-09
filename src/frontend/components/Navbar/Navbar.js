0b52ea690a9622c1e503ffd22349250f2f49cefe
Aidan Dempsey <aidandempsey2019@gmail.com>
2023-11-09 21:51:09 +0000

    update navbar

--- src/frontend/components/Navbar/Navbar.js
+++ src/frontend/components/Navbar/Navbar.js
@@ -1,15 +1,37 @@
+import { Link } from "react-router-dom"
+import { useOktaAuth } from "@okta/okta-react"
+
 // styles & images
 import "./Navbar.css"
 import Shield from "../../images/shield.svg"
 
 export default function Navbar() {
+
+    const { oktaAuth, authState } = useOktaAuth()
+
+    const handleLogout = async () => oktaAuth.signOut()
+
+    if (!authState) {
+        return (
+            <p>loading</p>
+        )
+    }
+
     return (
         <div className="navbar">
             <ul>
-                <li className="logo">
-                    <img src={Shield} alt="logo" />
-                    <span>OpenShield</span>
-                </li>
+
+                <li className="logo"><Link to="/"><img className="grow" src={Shield} alt="logo" /><span className="shield">OpenShield</span></Link></li>
+
+
+                {!authState.isAuthenticated ? (
+                    <>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/signup">Sign up</Link></li>
+                    </>
+                ) : <li><Link to="#" onClick={() => { handleLogout() }}>Logout</Link></li>
+
+                }
             </ul>
         </div>
     )
