808415958cec9f58681ba187d6593664440c019c
Aidan Dempsey <aidandempsey2019@gmail.com>
2023-11-18 00:27:36 +0000

    removing unneccesary files

--- src/main/java/me/adempsey/openshield/dao/WorkflowRepository.java
+++
@@ -1,7 +0,0 @@
-package me.adempsey.openshield.dao;
-
-import me.adempsey.openshield.entity.Workflow;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkflowRepository extends JpaRepository<Workflow, Long> {
-}
