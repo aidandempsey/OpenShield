7e28c888a74a9c5a61025e2a3d510fe5c47dc7f8
Aidan Dempsey <aidandempsey2019@gmail.com>
2023-12-02 23:45:22 +0000

    initializing device entity

--- src/main/java/me/adempsey/openshield/dao/DeviceRepository.java
+++ src/main/java/me/adempsey/openshield/dao/DeviceRepository.java
@@ -1,2 +1,11 @@
-package me.adempsey.openshield.dao;public class DeviceRepository {
+package me.adempsey.openshield.dao;
+
+import me.adempsey.openshield.entity.Device;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+    Page<Device> findByOrganizationId(@RequestParam("organization_id")Long organizationId, Pageable pageable);
 }
--- src/main/java/me/adempsey/openshield/entity/Device.java
+++ src/main/java/me/adempsey/openshield/entity/Device.java
@@ -1,2 +1,47 @@
-package me.adempsey.openshield.entity;public class Device {
+package me.adempsey.openshield.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="device")
+@Data
+public class Device {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="device_id")
+    private long deviceId;
+
+    @Column(name="device_name")
+    private String deviceName;
+
+    @Column(name="operating_system")
+    private String operatingSystem;
+
+    @Column(name="cpu_cores")
+    private long cpuCores;
+
+    @Column(name="cpu_threads")
+    private long cpuThreads;
+
+    @Column(name="memory_total")
+    private long memoryTotal;
+
+    @Column(name="memory_used")
+    private long memoryUsed;
+
+    @Column(name="memory_percent")
+    private float memoryPercent;
+
+    @Column(name="disk_total")
+    private long diskTotal;
+
+    @Column(name="disk_used")
+    private long diskUsed;
+
+    @Column(name="disk_percent")
+    private float diskPercent;
+
+    @Column(name="organization_id")
+    private long organizationId;
 }
