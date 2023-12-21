2c612ca2fe74cb9a53e6d0f84cc4378d0d2a88d4
Aidan Dempsey <aidandempsey2019@gmail.com>
2023-12-21 01:55:58 +0000

    filter incidents by severity

---
+++ src/frontend/pages/dashboard/IncidentFilter.js
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+export default function IncidentFilter(props) {
+    const filterList = ["All", "Low", "Medium", "High", "Critical"]
+    const [currentFilter, setCurrentFilter] = useState("all")
+    const { setSearch } = props
+
+    const handleClick = filter => {
+        const lowerFilter = filter.toLowerCase()
+        setCurrentFilter(lowerFilter)
+        let query = "incidents"
+        if (lowerFilter != "all") {
+            query = `incidents/search/findByIncidentSeverity?incidentSeverity=${lowerFilter}`
+        }
+        setSearch(query)
+    }
+
+    return (
+        <div className="incident-filter">
+            <nav>
+                <ul>
+                    {filterList.map(filter => (
+                        <button
+                            key={filter}
+                            onClick={() => { handleClick(filter) }}
+                            className={currentFilter === filter.toLowerCase() ? "active" : ""}>
+                            {filter}
+                        </button>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    )
+}
